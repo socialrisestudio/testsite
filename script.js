c40e3e75657f5772d700120215d611a8255be30c
socialrisestudio <145214045+socialrisestudio@users.noreply.github.com>
2024-01-17 15:53:09 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-fetch('https://ip-api.com/json/')
-.then(response => {
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-})
+fetch('https://ipinfo.io/json?token=d6a3cde87c2e63')
+.then(response => response.json())
 .then(data => {
     const locationElement = document.getElementById('user-location');
-    if(data && data.city) {
-        locationElement.textContent = data.city;
-    } else {
-        throw new Error('Location data is not available');
-    }
+    locationElement.textContent = data.city || 'your location'; // Fallback to 'your location' if city is not available
 })
-.catch(error => {
-    console.log('Error fetching or parsing the data:', error);
+.catch(() => {
     document.getElementById('user-location').textContent = 'your location';
 });
